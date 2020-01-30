2cb65eccad5596b85c4980cdeb068bad63332430
Yoseph Berhane <yosephbet@gmail.com>
2020-01-30 19:20:09 +0100

    committing added changes

--- index.js
+++ index.js
@@ -6,13 +6,11 @@ $(document).ready(function() {
 	var updateTimeLeft = function (amount) {
 		timeLeft += amount;
 		$('#time-left').text(timeLeft);
-	}
-	;
+	};
 	var updateScore = function (amount) {
 		score += amount;
 		$('#score').text(score);
-	}
-	;
+	};
 	var startGame = function () {
 		if (!interval) {
 			if (timeLeft === 0) {
@@ -32,24 +30,20 @@ $(document).ready(function() {
 	;
 	var randomNumberGenerator = function (size) {
 		return Math.ceil(Math.random() * size);
-	}
-	;
+	};
 	var questionGenerator = function () {
 		var question = {
-		}
-		;
+		};
 		var num1 = randomNumberGenerator(10);
 		var num2 = randomNumberGenerator(10);
 		question.answer = num1 + num2;
 		question.equation = String(num1) + " + " + String(num2);
 		return question;
-	}
-	;
+	};
 	var renderNewQuestion = function () {
 		currentQuestion = questionGenerator();
 		$('#equation').text(currentQuestion.equation);
-	}
-	;
+	};
 	var checkAnswer = function (userInput, answer) {
 		if (userInput === answer) {
 			renderNewQuestion();
@@ -57,18 +51,14 @@ $(document).ready(function() {
 			updateTimeLeft(+1);
 			updateScore(+1);
 		}
-	}
-	;
+	};
 	$('#user-input').on('keyup', function () {
 		startGame();
 		checkAnswer(Number($(this).val()), currentQuestion.answer);
-	}
-	);
+	});
 	$('#restart-button').on('click', function () {
 		startGame();
 		checkAnswer(Number($(this).val()), currentQuestion.answer);
-	}
-	);
+	});
 	renderNewQuestion();
-}
-);
+});
