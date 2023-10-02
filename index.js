f96cf6b99a5c857b3b8a59a9d322655ff5014e0d
Yoseph Berhane <yosephbet@gmail.com>
2023-10-02 14:04:52 +0200

    Refactor the code for better readability

--- index.js
+++ index.js
@@ -1,108 +1,90 @@
-$(document).ready(function(){
-  var currentQuestion;
-  var interval;
-  var timeLeft = 10;
-  var score = 0;
+document.addEventListener("DOMContentLoaded", function () {
+  const MAX_QUESTIONS = 10;
+  const COUNTDOWN_SECONDS = 10;
+  let currentQuestion;
+  let interval;
+  let timeLeft = COUNTDOWN_SECONDS;
+  let score = 0;
+  let questionsRemaining = MAX_QUESTIONS;
 
-  var updateTimeLeft = function (amount) {
+  const scoreElement = document.querySelector("#score");
+  const userInput = document.querySelector("#user-input");
+  const restartButton = document.querySelector("#restart-button");
+  const equationElement = document.querySelector(".equation");
+
+  const updateTimeLeft = (amount) => {
     timeLeft += amount;
-    $('#time-left').text(timeLeft);
   };
 
-  var updateScore = function (amount) {
+  const updateScore = (amount) => {
     score += amount;
-    $('#score').text(score);
+    scoreElement.textContent = score;
   };
 
-  var startGame = function () {
+  const startGame = () => {
     if (!interval) {
-      if (timeLeft === 0) {
-        updateTimeLeft(10);
-        updateScore(-score);
-      }
-      interval = setInterval(function () {
+      interval = setInterval(() => {
         updateTimeLeft(-1);
-        if (timeLeft === 0) {
+        if (timeLeft <= 0 || questionsRemaining <= 0) {
           clearInterval(interval);
-          interval = undefined;
+          endGame();
         }
       }, 1000);
+      renderNewQuestion();
     }
   };
 
-  var randomNumberGenerator = function (size) {
+  const randomNumberGenerator = (size) => {
     return Math.ceil(Math.random() * size);
   };
 
-  var questionGenerator = function () {
-    var question = {};
-    var num1 = randomNumberGenerator(10);
-    var num2 = randomNumberGenerator(10);
-
-    question.answer = num1 + num2;
-    question.equation = String(num1) + " + " + String(num2);
-
-    return question;
+  const questionGenerator = () => {
+    const num1 = randomNumberGenerator(10);
+    const num2 = randomNumberGenerator(10);
+    return {
+      answer: num1 + num2,
+      equation: `${num1} + ${num2}`,
+    };
   };
 
-  var renderNewQuestion = function () {
+  const renderNewQuestion = () => {
     currentQuestion = questionGenerator();
-    $('#equation').text(currentQuestion.equation);
+    equationElement.textContent = currentQuestion.equation;
   };
 
-  var checkAnswer = function (userInput, answer) {
-    if (userInput === answer) {
+  const checkAnswer = () => {
+    if (Number(userInput.value) === currentQuestion.answer) {
       renderNewQuestion();
-      $('#user-input').val('');
-      updateTimeLeft(+1);
-      updateScore(+1);
+      userInput.value = "";
+      updateTimeLeft(1);
+      updateScore(1);
+      questionsRemaining--;
     }
   };
 
-  $('#user-input').on('keyup', function () {
-    startGame();
-    checkAnswer(Number($(this).val()), currentQuestion.answer);
-  });
+  const endGame = () => {
+    userInput.disabled = true;
+    restartButton.disabled = false;
+  };
 
-  $('#restart-button').on('click', function () {
-    if (interval) {
-      clearInterval(interval);
+  userInput.addEventListener("keyup", function (event) {
+    if (event.key === "Enter") {
+      startGame();
+      checkAnswer();
     }
-    timeLeft = 10;
-    $('#time-left').text(timeLeft);
+  });
+
+  restartButton.addEventListener("click", function () {
+    clearInterval(interval);
+    interval = null;
+    timeLeft = COUNTDOWN_SECONDS;
     score = 0;
-    $('#score').text(score);
-    renderNewQuestion()
+    questionsRemaining = MAX_QUESTIONS;
+    scoreElement.textContent = score;
+    userInput.disabled = false;
+    restartButton.disabled = true;
+    startGame();
   });
 
-  // change the color of the equation
-  $('#equation').css('color', '#27ae60');
-  
-  // animate the time-left value on load
-  $('#time-left').animate({ fontSize: '100px' }, 1000, function() {
-    $(this).animate({ fontSize: '40px' }, 1000);
-  });
-  
-  // change the background color of the insertBox div
-  $('.insertBox').css('background-color', '#ecf0f1');
-  
-  // add hover effect on restart button
-  $('#restart-button').hover(function() {
-    $(this).css('background-color', '#8e44ad');
-  }, function() {
-    $(this).css('background-color', '#9b59b6');
-  });
-  
-  // add border to user-input field
-  $('#user-input').css('border', '2px solid #34495e');
-  
-  renderNewQuestion();
-});
-
-$(document).ready(function () {
-  var currentDate = new Date();
-  var day = currentDate.getDate();
-  var month = currentDate.getMonth() + 1;
-  var year = currentDate.getFullYear();
-  $("#current-date").text(month + "/" + day + "/" + year);
+  startGame();
 });
